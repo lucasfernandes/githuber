948f81fecb0651e36f69f24be4d6ad1ce383ba70
Lucas Silveira <skeletor@Lucass-MacBook-Pro.local>
2017-11-15 14:55:14 -0200

    adjusting routes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Welcome from 'pages/welcome';
 import Repositories from 'pages/repositories';
 import Organizations from 'pages/organizations';
 
-const createRootNavigator = (userExists = false) => 
+const createRootNavigator = (userExists = false) =>
   StackNavigator({
     Welcome: { screen: Welcome },
     User: {
@@ -14,7 +14,6 @@ const createRootNavigator = (userExists = false) =>
       }),
     },
   }, {
-    // initialRouteName: 'Welcome',
     initialRouteName: userExists ? 'User' : 'Welcome',
   });
 
